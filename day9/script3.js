95049fbd77bd861a1ef3a2e47f3d2339a21c2748
garvitnandawat <garvitjain.068gj@gmail.com>
2024-08-22 14:11:19 +0530

    Sleeping

---
+++ day9/script3.js
@@ -0,0 +1,19 @@
+function checkage() {
+    let age = document.getElementById("userInput").value;
+    age = parseFloat(age);
+    let result;
+
+    if (age >= 18) {
+        result = "you are eligible for vote";
+    } else if (age < 18) {
+        result = "you are not eligible for vote";
+    } else {
+        result = "Invalid Input";
+    }
+
+    document.getElementById("result").innerText = result;
+}
+
+const slicedOutput = arr.slice(1,5);
+console.log(slicedOutput);
+console.log(arr);
