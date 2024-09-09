85aeb72828df1ca0f3da8e91bd4ba651ac554dbd
garvitnandawat <garvitjain.068gj@gmail.com>
2024-09-09 09:28:56 +0530

    .

--- Node/server.js
+++ Node/server.js
@@ -1,18 +1,31 @@
-const { log } = require('console');
-const { response } = require('express');
-const http = require('http') ;
+const http = require('http');
+const path = require('path');
+const fs = require('fs');
+const server = http.createServer((req, res) => {
+  console.log(req.url);
+  if (req.url === '/about' && req.method === 'GET') {
+    const filePath = path.join(__dirname, 'about.html');
+    fs.readFile(filePath, (err, data) => {
+      if (err) {
+        res.end('err');
+      } else {
+        res.writeHead(200,{'Content-Type':'text/html'})
+        res.end(data);
+      }
+    });
 
-const server = http.createServer((req,res)=>
-{
-    res.end("data is send from the server")
-})
+    //NOTE always use writeHead method before sending the response
+  } else if (req.url === '/services') {
+    res.end('service page');
+  } else {
+    res.end('home page'); 
+  }
+});
 
-server.listen(3000,()=>{
-    console.log('server is running');
-    
-})
+server.listen(4000, () => {
+  console.log('Server is running');
+});
 
-http => request , response
+// http => request  , response
 
-
-console.log('hello hello hello');
+// npm i nodemon -g --save-dev
